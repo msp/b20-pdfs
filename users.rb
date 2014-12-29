71a51dab29a6c948bade0a62b683f1ae0099c075
msp <matt@cenatus.org>
2014-12-29 20:05:03 +0000

    Final PDF and increase copies available.

--- users.rb
+++ users.rb
@@ -6,7 +6,7 @@ require 'ffaker'
 
 out = File.new "users.yml", "w"
 
-(1..280).each do |i|
+(1..700).each do |i|
     user =  {"user_#{i}" => ["username" => Faker::Internet.user_name(Faker::HipsterIpsum.words(2).to_s[0..19])+rand(9).to_s, "password" => Faker::Internet.password[0..7]] }
     user.to_yaml(out)
 end
